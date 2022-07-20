b7fc24570e37d82ee9cecce9b53934a2d6b50d76
SamerZehra <105205353+SamerZehra@users.noreply.github.com>
2022-07-20 12:19:27 +0530

    Add files via upload

--- src/main/java/TypewiseAlert/BatteryCharacter.java
+++ src/main/java/TypewiseAlert/BatteryCharacter.java
@@ -1,7 +1,5 @@
 package typewisealert;
 
-import javax.swing.text.AbstractDocument.BranchElement;
-
 import typewisealert.constants.BreachType;
 import typewisealert.constants.CoolingType;
 
@@ -9,22 +7,8 @@ public class BatteryCharacter {
 	public CoolingType coolingType;
 	public static String brand;
     
-    public static BreachType classifyTemperatureBreach(CoolingType coolingType ,double temperatureInC) {
-		int lowerLimit = 0;
-		int upperLimit = 0;
-		switch (coolingType) {
-		case PASSIVE_COOLING:
-			upperLimit = 35;
-			break;
-		case HI_ACTIVE_COOLING:
-			upperLimit = 45;
-			break;
-		case MED_ACTIVE_COOLING:
-			upperLimit = 40;
-			break;
-		}
-		brand = coolingType.name();
-		System.out.println("Brand : " + brand);
-		return BreachType.inferBreach(temperatureInC, lowerLimit, upperLimit);
+	public BatteryCharacter(CoolingType coolingType, String brand) {
+		this.coolingType = coolingType;
+		this.brand = brand;
 	}
 }
