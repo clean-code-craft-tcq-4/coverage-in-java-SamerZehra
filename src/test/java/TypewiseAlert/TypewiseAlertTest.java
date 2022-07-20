1ba1308b20eb1914c85f88ad37531df57726d1e0
SamerZehra <105205353+SamerZehra@users.noreply.github.com>
2022-07-20 12:30:45 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,15 +1,48 @@
 package TypewiseAlert;
 
+
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class TypewiseAlertTest 
-{
-    @Test
-    public void infersBreachAsPerLimits()
-    {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
-    }
+
+import Typewisealert.BatteryCharacter;
+import Typewisealert.TypewiseAlert;
+import Typewisealert.constants.AlertTarget;
+import Typewisealert.constants.BreachType;
+import Typewisealert.constants.CoolingType;
+
+public class TypewiseAlertTest {
+	
+	@Test
+	public void infersBreachAsPerLimits() {
+		assertTrue(BreachType.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
+		assertTrue(BreachType.inferBreach(40, 30, 40) == BreachType.NORMAL);
+		assertTrue(BreachType.inferBreach(55, 45, 50) == BreachType.TOO_HIGH);
+	}
+	
+	@Test
+	public void verifyBreachType() {
+		assertTrue(TypewiseAlert.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, -10) == BreachType.TOO_LOW);
+		assertTrue(TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 15) == BreachType.NORMAL);
+		assertTrue(TypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 55) == BreachType.TOO_HIGH);
+	}
+	 @Test
+	 public void verifyEmailHighAlertSent() {
+		 BatteryCharacter batteryChar = new BatteryCharacter(CoolingType.HI_ACTIVE_COOLING, "bosch");
+		 assertTrue(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryChar, 55) == 1);
+		 
+	 }
+	 
+	 @Test
+	 public void verifyEmailLowAlertSent() {
+		 BatteryCharacter batteryChar = new BatteryCharacter(CoolingType.PASSIVE_COOLING, "bosch");
+		 assertTrue(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryChar, -10) == 1);
+	 }
+	 
+	 @Test
+	 public void verifyControllerAlertSent() {
+		 BatteryCharacter batteryChar = new BatteryCharacter(CoolingType.PASSIVE_COOLING, "bosch");
+		 assertTrue(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryChar, -10) == 1);
+	 }
 }
