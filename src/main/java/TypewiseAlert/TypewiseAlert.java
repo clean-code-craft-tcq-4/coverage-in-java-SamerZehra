9ad2ad0ac203c978e2583258229c863a5741c764
SamerZehra <105205353+SamerZehra@users.noreply.github.com>
2022-07-20 12:18:59 +0530

    Update TypewiseAlert.java

--- src/main/java/TypewiseAlert/TypewiseAlert.java
+++ src/main/java/TypewiseAlert/TypewiseAlert.java
@@ -6,14 +6,27 @@ import typewisealert.constants.CoolingType;
 
 public class TypewiseAlert {
     
-	public static void checkAndAlert(AlertTarget alertTarget, CoolingType coolingType, double temperatureInC) {
+	public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+		int lowerLimit = 0;
+		int upperLimit = 0;
+		if (coolingType != null) {
+			lowerLimit = coolingType.getTempLowerLimit();
+			upperLimit = coolingType.getTempUpperLimit();
+		}
+		return BreachType.inferBreach(temperatureInC, lowerLimit, upperLimit);
+	}
+	
+	public static int checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-		BreachType breachType = BatteryCharacter.classifyTemperatureBreach(coolingType ,temperatureInC);
+		BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType ,temperatureInC);
 		
 		if(alertTarget == AlertTarget.TO_CONTROLLER) {
 			Alerter.sendToController(breachType);
+			return 1;
+			
 		}else {
 			Alerter.sendToEmail(breachType);
+			return 1;
 		}
 	}
 	
